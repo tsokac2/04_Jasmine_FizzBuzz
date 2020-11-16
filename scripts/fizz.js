6dfba4850e55ad78d36a2cc932a986c5526af495
tsokac2 <tsokac2@gmail.com>
2020-11-16 19:08:25 +0000

    Uploading index.html, scriprz/fizz.js and specs/fizzSpecs.js

---
+++ scripts/fizz.js
@@ -0,0 +1,3 @@
+function FizzBuzz (num) {
+    return num;
+};
---
+++ spec/fizzSpecs.js
@@ -0,0 +1,14 @@
+describe("FizzBuzz", function (){
+
+    beforeEach(function(){
+        fizz = new FizzBuzz();
+    });
+
+    describe("FizzBuzz test", function(){
+
+        it("FizzBuzz function should exist", function(){
+            expect(FizzBuzz).toBeDefined();
+        });
+        
+    });
+});
